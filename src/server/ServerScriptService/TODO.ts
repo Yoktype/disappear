14f0472fb8ae12ab3b2f7d108f473245bc4cf5c0
Yok <233425415+Yoktype@users.noreply.github.com>
2025-10-31 01:43:06 +0300

    init

---
+++ src/server/ServerScriptService/TODO.ts
@@ -0,0 +1,20 @@
+/*
+
+
+
+BugReports:{
+    -
+}
+
+TODO:{
+    - git init and connect repository
+    - JSON files ignored (rojo) Assets, Events
+    - Player, data save profile store create template
+    - CombatV1
+    - OOP
+    - React
+}
+
+
+
+*/
